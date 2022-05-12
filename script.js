89db2866de6e1808b2166bc191be6dd7a004ca3d
Danny Carrington <104363631+dannycarrington@users.noreply.github.com>
2022-05-12 13:15:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+function toggleNav() {
+  var wrapper = document.getElementById("wrapper");
+  
+  if(wrapper.classList.contains("open")){
+    wrapper.classList.remove("open")
+  } else {
+    wrapper.classList.add("open");
+  };
 }
