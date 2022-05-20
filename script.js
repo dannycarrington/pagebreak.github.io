63ba1549faed7e46449e6381dd2c2a7db3c3e964
Danny Carrington <104363631+dannycarrington@users.noreply.github.com>
2022-05-20 12:02:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function toggleNav() {
+  var wrapper = document.getElementById("wrapper");
+
+  if (wrapper.classList.contains("open")) {
+    wrapper.classList.remove("open")
+  } else {
+    wrapper.classList.add("open");
+  };
+}
+const isPrime = num => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+    if (num % i === 0) return false;
+  return num > 1;
+}
+for (i = 0; i <= 90; i++) {
+  if (i == 54) { // the 69th button
+    document.getElementById("puzzle").innerHTML += `<button class="our-btn" onClick="alert('HAH! You though you got lucky huh?')">Button</button>`
+  }
+  else { // the normal buttondiv 
+    color = "black"
+    if (i == 61){
+      color = "red"
+    }
+    else if (isPrime(i)) {
+      color = "blue"
+    }
+    else if (i % 2){
+      color = "red"
+    }
+    else if (i % 5){
+      color = "green"
+    }
+      document.getElementById("puzzle").innerHTML += `<button class="our-btn" style="border-color: ${color}"" onClick="alert('Could be this one?')">Button</button>`
+  }
+  
+}
+
+
+function handlebtn(){
+  let val = document.getElementById("value").value
+  if(val.toLowerCase() == "australia"){
+    document.location = "/winner.html"
+  } else {
+    alert("incorrect!")
+  }
+}
